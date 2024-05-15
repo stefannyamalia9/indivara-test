c25440f9ed62027bc57182feab179ab600ee5920
stefannyamalia <stefannyamalia.work@gmail.com>
2024-05-15 18:55:08 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+const [data, setData] = useState([])
+const[ searchValue, setSearchValue] = useState("")
+
+useEffect (()=> {
+  const fetchProduct = async () => {
+    try {
+      const response = await axios.get("https://fakestoreapi.com/products")
+      setData(response.data)
+      console.log(data)
+    } catch (error){
+      console.log(error)
+    }
+  }
+  fetchProduct()
+}, [])
+
+const searchProduct = (event) => {
+setSearchValue(event.target.value)
+
+console.log(searchValue)
+  setData(data.filter((product)=> (
+    product.title.includes(searchValue)
+  )))
+}
+
+const resetValue = () => {
+  setSearchValue('')
+  window.location.reload()
+}
+
+const sortPrice = () => {
+  const sortedProduct = [...data].sort((a, b) => b.price - a.price);
+  setData(sortedProduct);
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Data</h1>
+      <div className='button-group'>
+      <input type="text" value={searchValue} onChange={searchProduct} placeholder='Mohon perhatikan huruf kapital..'/>
+      <button onClick={resetValue}>Reset</button>
+      <button onClick={sortPrice}>Sort Price high-Low</button>
+      </div>
+      <div className='product-list'>
+      {
+        data.map((product)=>(
+          <div className='product-item'>
+            <li>{product.id}</li>
+            <li>{product.title}</li>
+            <li>{product.price}</li>
+          </div>
+        ))
+      }
+      </div>
     </div>
   );
 }
